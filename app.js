f6f0e5029cd9aa3e00d99e4955506cbd3215d05c
Lameck <lameck2@hotmail.com>
2019-07-01 16:26:24 -0300

    first commit: Adding dependencies and hello world

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
